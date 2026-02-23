fc749062eb182fcbe01184b5f9d59684c228774b
Saloni Mhatre <mhatresaloni050@gmail.com>
2026-02-23 10:54:03 +0530

    Removed old unused files

--- mongo_test.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017")
-db = client["test_db"]
-
-db.test.insert_one({"msg": "MongoDB connected"})
-print("MongoDB connection SUCCESS")
